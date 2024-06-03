39adefb47f4b0367eefc5b673d90336ec62cb4aa
yurii <robka4an@gmail.com>
2024-06-03 16:54:16 +0300

    finish first project

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+const images = [
+  "https://images.pexels.com/photos/3836292/pexels-photo-3836292.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=200",
+  "https://images.pexels.com/photos/2792157/pexels-photo-2792157.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=200",
+  "https://images.pexels.com/photos/1166209/pexels-photo-1166209.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=200",
+  "https://images.pexels.com/photos/1661179/pexels-photo-1661179.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=200",
+  "https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=200",
+];
+
+export default function App() {
+  const [img, setImg] = useState(0);
+
+  const next = () => {
+    setImg(img + 1);
+  };
+  const prev = () => {
+    setImg(img - 1);
+  };
+  return (
+    <div>
+      {img !== 0 && <button onClick={prev}>Prev</button>}
+      <img src={images[img]} />
+      {img < 4 && <button onClick={next}>Next</button>}
+    </div>
+  );
+}
